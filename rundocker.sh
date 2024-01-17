da5543a07bf0ba8618de3e8c1df713705bfbfe1f
tuneman7 <tuneman7@berkeley.edu>
2024-01-16 22:28:37 -0800

    periodic update

--- rundocker.sh
+++ rundocker.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
-docker build -t delta_quickstart -f Dockerfile_delta_quickstart .
+# Function to check if docker image exists
+check_docker_image() {
+    docker image inspect $1 > /dev/null 2>&1
+    return $?
+}
 
-# Build entry point
-docker run --name delta_quickstart --rm -it -p 8888-8889:8888-8889 delta_quickstart
+# Image name
+IMAGE_NAME="delta_quickstart"
 
-server_ports_used=("8889")
+# Check if the Docker image already exists
+if check_docker_image $IMAGE_NAME; then
+    echo "Docker image $IMAGE_NAME already exists. Do you want to rebuild it? [y/N]"
+    
+    # Read user input, timeout after 2 seconds, default to No
+    read -t 2 -p "Rebuild? (y/N): " REBUILD
+    REBUILD=${REBUILD:-n}
+
+    # Rebuild docker image if user input is 'y'
+    if [[ $REBUILD =~ ^[Yy]$ ]]; then
+        docker build -t $IMAGE_NAME -f Dockerfile_delta_quickstart .
+    fi
+else
+    # Build the docker image if it doesn't exist
+    docker build -t $IMAGE_NAME -f Dockerfile_delta_quickstart .
+fi
+
+# Run the Docker container in background
+docker run --name $IMAGE_NAME --rm -p 8888-8889:8888-8889 $IMAGE_NAME
+
+echo "Docker container $IMAGE_NAME is running in background."
+
+# Prompt for teardown
+read -p "Do you want to teardown the Docker container? (y/N): " TEARDOWN
+
+if [[ $TEARDOWN =~ ^[Yy]$ ]]; then
+    # Stop and remove the Docker container
+    docker stop $IMAGE_NAME
+
+    echo "Docker container $IMAGE_NAME has been stopped and removed."
+
+    # Additional teardown steps can be added here
+fi
+
+server_ports_used=("8888")
 
 
-#check if the user wants to exit
     #If yess, kill all the processes running fastapi / guinicorn
     for m_port in "${server_ports_used[@]}"
     do
